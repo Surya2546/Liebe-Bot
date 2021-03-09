19dbc6f6e5cc64ec8cdef034dd5903caa59e5574
Shinchan-xD <77878032+Shinchan-xD@users.noreply.github.com>
2021-03-09 15:20:36 +0530

    Update feds.py

--- miley/modules/feds.py
+++ miley/modules/feds.py
@@ -1685,7 +1685,7 @@ Ah, group management. Everything is fun, until the spammer starts entering your 
 But then you have many groups, and you don't want this spammer to be in one of your groups - how can you deal? Do you have to manually block it, in all your groups?
 No longer! With Federation, you can make a ban in one chat overlap with all other chats.
 You can even designate admin federations, so your trusted admin can ban all the chats you want to protect.
-For help and updates visit @Liebe_News and Join @Liebe_Support aslo 🙂
+For help and updates visit @Liebe__News and Join @Liebe__Support aslo 🙂
 Command:
  - /newfed <fedname>: Create a new Federation with the name given. Users are only allowed to have one Federation. This method can also be used to rename the Federation. (max. 64 characters)
  - /delfed: Delete your Federation, and any information related to it. Will not cancel blocked users.
