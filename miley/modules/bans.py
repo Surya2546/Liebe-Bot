946efdc47ddf88422a97f02704f65d0c98dfa0fc
Shinchan-xD <77878032+Shinchan-xD@users.noreply.github.com>
2021-03-21 10:07:50 +0530

    Update bans.py

--- miley/modules/bans.py
+++ miley/modules/bans.py
@@ -416,7 +416,7 @@ __help__ = """
  - /kick <userhandle>: kicks a user, (via handle, or reply)
 """
 
-__mod_name__ = "BANS"
+__mod_name__ = "BANS🚫"
 
 BAN_HANDLER = CommandHandler("ban", ban, pass_args=True, filters=Filters.group)
 TEMPBAN_HANDLER = CommandHandler(["tban", "tempban"], temp_ban, pass_args=True, filters=Filters.group)
