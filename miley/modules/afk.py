27272fb33aa7b919f40602599dc0ab429125baca
Shinchan-xD <77878032+Shinchan-xD@users.noreply.github.com>
2021-03-21 11:01:42 +0530

    Update afk.py

--- miley/modules/afk.py
+++ miley/modules/afk.py
@@ -138,8 +138,7 @@ dispatcher.add_handler(AFK_REGEX_HANDLER, AFK_GROUP)
 dispatcher.add_handler(NO_AFK_HANDLER, AFK_GROUP)
 dispatcher.add_handler(AFK_REPLY_HANDLER, AFK_REPLY_GROUP)
 
-__mod_name__ = "𝓐𝓕𝓚
-"
+__mod_name__ = "𝓐𝓕𝓚"
 __command_list__ = ["afk"]
 __handlers__ = [(AFK_HANDLER, AFK_GROUP), (AFK_REGEX_HANDLER, AFK_GROUP), (NO_AFK_HANDLER, AFK_GROUP),
                 (AFK_REPLY_HANDLER, AFK_REPLY_GROUP)]
