4eb434916bffae7e44d45016e07cb667334da809
Shinchan-xD <77878032+Shinchan-xD@users.noreply.github.com>
2021-03-21 10:17:45 +0530

    Update cust_filters.py

--- miley/modules/cust_filters.py
+++ miley/modules/cust_filters.py
@@ -308,7 +308,7 @@ doin?
  - /stopall: stop all filters
 """
 
-__mod_name__ = "Filters"
+__mod_name__ = "Filters📢"
 
 FILTER_HANDLER = CommandHandler("filter", filters)
 STOP_HANDLER = CommandHandler("stop", stop_filter)
