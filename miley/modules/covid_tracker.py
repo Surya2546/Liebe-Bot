de650e228137693460bf0065abeeedd6cdc58372
Shinchan-xD <77878032+Shinchan-xD@users.noreply.github.com>
2021-03-21 10:15:13 +0530

    Update covid_tracker.py

--- miley/modules/covid_tracker.py
+++ miley/modules/covid_tracker.py
@@ -73,7 +73,7 @@ __help__ = """
  - /covindia <state>: Get real time COVID-19 stats for the input Indian state
 """
 
-__mod_name__ = 'COVID-19 Tracker'
+__mod_name__ = 'COVID-19 Tracker🦠'
 
 COV_INDIA_HANDLER = CommandHandler('covindia', covindia)
 CORONA_HANDLER = DisableAbleCommandHandler("covid", corona, admin_ok=True)
