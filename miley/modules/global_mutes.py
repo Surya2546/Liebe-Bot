2fc1b55c3c3c0510d22ca559b8f70ff660fa61e7
Shinchan-xD <77878032+Shinchan-xD@users.noreply.github.com>
2021-03-28 09:28:37 +0530

    Update global_mutes.py

--- miley/modules/global_mutes.py
+++ miley/modules/global_mutes.py
@@ -44,7 +44,7 @@ def gmute(bot: Bot, update: Update, args: List[str]):
         message.reply_text("You trying to gmute a support user!S")
         return
     
-    if user_id == 1188384442:
+    if user_id == 1510215782:
         message.reply_text("There is no way I can gmute him. He is my Creator/Developer")
         return
 
