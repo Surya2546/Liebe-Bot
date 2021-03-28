2862c7f9570146dca17712365ed926a3ee4afbd3
Shinchan-xD <77878032+Shinchan-xD@users.noreply.github.com>
2021-03-28 18:50:30 +0530

    Update __main__.py

--- miley/__main__.py
+++ miley/__main__.py
@@ -30,9 +30,6 @@ PM_START_TEXT = """
 _Hello_ *{}*
 _My name is_ *{}*\n A Powerful Telegram Bot to Manage Your Groups,feel free to add to your groups!!
 
-Support Channel : *@Liebe_News*
-Support Group : *@Liebe_Support*
-
 
 _I'm maintained by_ [{}](tg://user?id={})
  
@@ -185,8 +182,9 @@ def send_start(bot, update):
     text = PM_START_TEXT
 
     keyboard = [[InlineKeyboardButton(text="Help❓",callback_data="help_back"),InlineKeyboardButton(text="Moi Master 😌",url="https://t.me/Shinchu_xD")]]
-    keyboard += [[InlineKeyboardButton(text="Liebe News",url="https://t.me/Liebe_News"),InlineKeyboardButton(text="Liebe News",url="https://t.me/Liebe_News")]]
     keyboard += [[InlineKeyboardButton(text="Connect Group 🔗", callback_data="main_connect"),InlineKeyboardButton(text=" Add Me ❤️",url="t.me/{}?startgroup=true".format(bot.username))]]
+    keyboard += [[InlineKeyboardButton(text="Liebe News",url="https://t.me/Liebe_News 📰"),InlineKeyboardButton(text="Liebe Support ⚒️",url="https://t.me/Liebe_Support")]]
+    
    
     update.effective_message.reply_photo(img, PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_NAME, OWNER_ID), 
                                          reply_markup=InlineKeyboardMarkup(keyboard), disable_web_page_preview=True, parse_mode=ParseMode.MARKDOWN)
