049d3e88a6ca5cbd8abdf686d87da6993c75a654
Shinchan-xD <77878032+Shinchan-xD@users.noreply.github.com>
2021-03-28 10:03:34 +0530

    Update __main__.py

--- miley/__main__.py
+++ miley/__main__.py
@@ -170,7 +170,7 @@ def start(bot: Bot, update: Update, args: List[str]):
         else:
             send_start(bot, update)
     else:
-        update.effective_message.reply_text("Heya,{} Here..\nHow can I help you? 🙂".format(bot.first_name),reply_markup=InlineKeyboardMarkup(
+        update.effective_message.reply_text("Heya,{} Here..\nHow can I help you? 😎".format(bot.first_name),reply_markup=InlineKeyboardMarkup(
                                                 [[InlineKeyboardButton(text="⚜️Help",url="t.me/{}?start=help".format(bot.username))]]))
 
 def send_start(bot, update):
@@ -185,7 +185,7 @@ def send_start(bot, update):
     first_name = update.effective_user.first_name 
     text = PM_START_TEXT
 
-    keyboard = [[InlineKeyboardButton(text="🤝Help",callback_data="help_back"),InlineKeyboardButton(text="🔥Creator🔥",url="https://t.me/Shinchu_xD")]]
+    keyboard = [[InlineKeyboardButton(text="🔱Help🔱",callback_data="help_back"),InlineKeyboardButton(text="🔥Creator🔥",url="https://t.me/Shinchu_xD")]]
     keyboard += [[InlineKeyboardButton(text="🌐Connect Group", callback_data="main_connect"),InlineKeyboardButton(text="✳ Add Me ✳",url="t.me/{}?startgroup=true".format(bot.username))]]
 
     update.effective_message.reply_photo(img, PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_NAME, OWNER_ID), 
