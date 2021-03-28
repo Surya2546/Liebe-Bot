04f85763e3b842a4250a461b36bd4f0efe188a11
Shinchan-xD <77878032+Shinchan-xD@users.noreply.github.com>
2021-03-28 09:52:10 +0530

    Update __main__.py

--- miley/__main__.py
+++ miley/__main__.py
@@ -185,7 +185,7 @@ def send_start(bot, update):
     first_name = update.effective_user.first_name 
     text = PM_START_TEXT
 
-    keyboard = [[InlineKeyboardButton(text="🤝Help",callback_data="help_back"),InlineKeyboardButton(text="🔥Creator🔥",url="https://t.me/Mafia_Shinchan_Op")]]
+    keyboard = [[InlineKeyboardButton(text="🤝Help",callback_data="help_back"),InlineKeyboardButton(text="🔥Creator🔥",url="https://t.me/Shinchu_xD")]]
     keyboard += [[InlineKeyboardButton(text="🌐Connect Group", callback_data="main_connect"),InlineKeyboardButton(text="✳ Add Me ✳",url="t.me/{}?startgroup=true".format(bot.username))]]
 
     update.effective_message.reply_photo(img, PM_START_TEXT.format(escape_markdown(first_name), escape_markdown(bot.first_name), OWNER_NAME, OWNER_ID), 
